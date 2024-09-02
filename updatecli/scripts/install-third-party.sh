3515ffa9095894e24914ae42cfa447b195b824c5
José Guilherme Vanz <jguilhermevanz@suse.com>
2024-09-02 15:09:33 -0300

    feat(ci): script to update third_party directory.
    Adds a new updatecli script to update the third_party directory files
    with the latest releases of the apiserver and apimachinery.
    
    Signed-off-by: José Guilherme Vanz <jguilhermevanz@suse.com>
    
    Signed-off-by: José Guilherme Vanz <jguilhermevanz@suse.com>

---
+++ updatecli/scripts/install-third-party.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Check if the base directory exists
+if [ ! -d "$REPOSITORY_DIR" ]; then
+    echo "Error: Base directory $base_directory does not exist."
+    exit 1
+fi
+
+# Check if the destination directory exists
+if [ ! -d "$DESTINATION_DIR" ]; then
+    echo "Error: Destination directory $destination_directory does not exist."
+    exit 1
+fi
+
+
+# Loop through each file in the list
+for file in $FILES; do
+    # Find the file in the base directory
+    file_path="$REPOSITORY_DIR/$file"
+    
+    if [ -n "$file_path" ]; then
+        # Use the install command to copy the file and create the necessary directories
+        install -m "644" -D "$file_path" "$DESTINATION_DIR/$file"
+        echo "Copied $file_path to $DESTINATION_DIR/$file"
+    else
+        echo "File '$file_path' not found in $REPOSITORY_DIR."
+	exit 1
+    fi
+done
+
+echo "Done!"
